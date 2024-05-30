56ab4d3ba31fda36c23f6ab67f160c712138a80c
MysteriousRob <robertjohngasior@gmail.com>
2024-05-30 16:17:51 +0200

    made hospitality industry experience into disclosures works

--- src/Pages/about me/partials/ProfessionalStory.jsx
+++ src/Pages/about me/partials/ProfessionalStory.jsx
@@ -30,7 +30,109 @@ function ProfessionalStory() {
                       className={clsx("w-5", innerOpen && "rotate-180")}
                     />
                   </DisclosureButton>
-                  <DisclosurePanel>No</DisclosurePanel>
+                  <DisclosurePanel>
+                    {" "}
+                    <p className="pl-[100px] indent-2">
+                      I spent five years working in the restaurant industry,
+                      gaining a wide range of skills and experiences:
+                    </p>
+                  </DisclosurePanel>
+                  <DisclosurePanel>
+                    <Disclosure>
+                      {({ open }) => (
+                        <>
+                          <DisclosureButton className="flex items-center gap-2">
+                            <li className="pl-[150px]">
+                              <span className="text-black font-bold">
+                                Waiter:
+                              </span>
+                            </li>
+                            <ChevronDownIcon
+                              className={clsx("w-5", open && "rotate-180")}
+                            />
+                          </DisclosureButton>
+                          <DisclosurePanel>
+                            <li className="pl-[190px]">
+                              Initially started as a waiter, where I honed my
+                              customer service skills and learned the
+                              intricacies of the hospitality sector.
+                            </li>
+                          </DisclosurePanel>
+                        </>
+                      )}
+                    </Disclosure>
+                    <Disclosure>
+                      {({ open }) => (
+                        <>
+                          <DisclosureButton className="flex items-center gap-2">
+                            <li className="pl-[150px]">
+                              <span className="text-black font-bold">
+                                Bartender:
+                              </span>
+                            </li>
+                            <ChevronDownIcon
+                              className={clsx("w-5", open && "rotate-180")}
+                            />
+                          </DisclosureButton>
+                          <DisclosurePanel>
+                            <li className="pl-[190px]">
+                              Progressed to bartending, mastering the art of
+                              mixing drinks and providing exceptional service to
+                              guests.
+                            </li>
+                          </DisclosurePanel>
+                        </>
+                      )}
+                    </Disclosure>
+                    <Disclosure>
+                      {({ open }) => (
+                        <>
+                          <DisclosureButton className="flex items-center gap-2">
+                            <li className="pl-[150px]">
+                              <span className="text-black font-bold">
+                                Mixologist:
+                              </span>
+                            </li>
+                            <ChevronDownIcon
+                              className={clsx("w-5", open && "rotate-180")}
+                            />
+                          </DisclosureButton>
+                          <DisclosurePanel>
+                            <li className="pl-[190px]">
+                              Eventually became a mixologist, responsible for
+                              crafting innovative cocktail and mocktail menus
+                              that delighted patrons and enhanced the
+                              establishment's reputation.
+                            </li>
+                          </DisclosurePanel>
+                        </>
+                      )}
+                    </Disclosure>
+                    <Disclosure>
+                      {({ open }) => (
+                        <>
+                          <DisclosureButton className="flex items-center gap-2">
+                            <li className="pl-[150px]">
+                              <span className="text-black font-bold">
+                                Bar Management:
+                              </span>
+                            </li>
+                            <ChevronDownIcon
+                              className={clsx("w-5", open && "rotate-180")}
+                            />
+                          </DisclosureButton>
+                          <DisclosurePanel>
+                            <li className="pl-[190px]">
+                              Often took on bar management responsibilities in
+                              the absence of the main manager, ensuring smooth
+                              operations and maintaining high standards of
+                              service.
+                            </li>
+                          </DisclosurePanel>
+                        </>
+                      )}
+                    </Disclosure>
+                  </DisclosurePanel>
                 </>
               )}
             </Disclosure>
@@ -45,9 +147,7 @@ function ProfessionalStory() {
                       className={clsx("w-5", open && "rotate-180")}
                     />
                   </DisclosureButton>
-                  <DisclosurePanel>
-                    
-                  </DisclosurePanel>
+                  <DisclosurePanel></DisclosurePanel>
                 </>
               )}
             </Disclosure>
